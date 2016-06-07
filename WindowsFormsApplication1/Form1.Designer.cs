55e2e21dd21cc2e2b89eeb1b3ecf421acede0afa
Ivan <john.matyash@gmail.com>
2016-06-07 15:51:22 +0300

    ver 1.0

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PrjScr search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(450, 495);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\TestRun\\new_test\\TestBed";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ".ini search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(341, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "C:\\TestRun\\new_test\\test.ini";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(175, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "C:/TestRun/a.txt";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(221, 68);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(175, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "C:/TestRun/b.txt";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(402, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "diff";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(195, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 22);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "⇄";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 600);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AutoTest Finder 3000 Deluxe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string st = "";
+            try
+            {
+                //string[] dirs = Directory.GetFiles(@"C:\TestRun\new_test\TestBed", "*.PrjScr", SearchOption.AllDirectories);
+                string[] dirs = Directory.GetFiles(textBox1.Text.ToString(), "*.PrjScr", SearchOption.AllDirectories);
+
+                foreach (string dir in dirs)
+                {
+                    st += Path.GetFileName(dir).Split('.')[0] + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                st += ("The process failed: {0}" + ex.ToString());
+            }
+            richTextBox1.Text = st.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //using (StreamReader str = new StreamReader(@"C:\TestRun\new_test\test.ini", Encoding.Default))
+            using (StreamReader str = new StreamReader(textBox2.Text.ToString(), Encoding.Default))
+            {
+                string txtFile = str.ReadToEnd();
+
+                richTextBox1.Text = GetText(txtFile);
+            }
+        }
+        public string GetText(string str)
+        {
+            string res = "";
+            Regex pattern =
+                new Regex(@"\[(?<val>.*?)\]",
+                    RegexOptions.Compiled |
+                    RegexOptions.Singleline);
+
+            foreach (Match m in pattern.Matches(str))
+                if (m.Success)
+                {
+                    res += (m.Groups["val"].Value) + "\n";
+                }
+            return res;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = textBox3.Text.ToString();
+            string b = textBox4.Text.ToString();            
+            int count = 0;
+
+            var mas1 = File.ReadAllLines(textBox3.Text.ToString()).ToArray();
+            var mas2 = File.ReadAllLines(textBox4.Text.ToString()).ToArray();
+
+            List<string> keywords2 = mas1.Except(mas2).ToList<string>();
+
+            richTextBox1.Text = "Files that are present in " + GetFileName(a) + " and are absent in " + GetFileName(b) + ":\n\n";                  
+            
+            foreach (string s in keywords2)
+            {
+                richTextBox1.Text += s + "\n";
+                count++;
+            }
+            richTextBox1.Text += "\nTotal count: "+ count;
+        }
+
+        private string GetFileName(string Path)
+        {
+            string newVal = null;
+            for (int i = Path.Length - 1; i > 0; i--)
+            {
+                if (Path[i] == '/')
+                {
+                    break;
+                }
+                newVal += Path[i];
+            }
+            var tt = newVal.ToCharArray();
+            Array.Reverse(tt);
+            return new string(tt);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tmp ="";
+            tmp = textBox3.Text.ToString();
+            textBox3.Text = textBox4.Text.ToString();
+            textBox4.Text = tmp;
+        }
+    }
+}
+    
